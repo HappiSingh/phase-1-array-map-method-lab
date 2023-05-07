c522473d150095179d4b14689202c20e1edfafb6
Harpreet Singh <Harpreets1201@gmail.com>
2023-05-07 19:19:14 -0400

    Completed lab

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((string) => {
+    const wordArray = string.split(" ");
+    const newWords = wordArray.map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return newWords.join(" ");
+  });
+};
+
